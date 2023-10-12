d2792d3c70dcab317aa1e205c9061a79df15e8d2
Baptiste Lhopitault <baptiste@repliq.fr>
2023-10-12 14:56:54 +0200

    Update textes

--- languages/fr.php
+++ languages/fr.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    "field.blocks.gallery.name" => "Gallerie",
+    "field.blocks.gallery.name" => "Galerie",
     "field.blocks.gallery.layout.label" => "Nombres d'image par ligne",
     "field.blocks.gallery.images.label" => "Images",
     "field.blocks.gallery.images.imagelabel" => "Image",
     "field.blocks.gallery.images.imagealt" => "Texte alternatif",
-    "field.blocks.gallery.images.imagealthelp" => "Texte permettant de comprendre l'image sans la voir. C'est pour le référencement et les personnes porteuses de handicaps.",
+    "field.blocks.gallery.images.imagealthelp" => "Texte permettant de comprendre l'image sans la voir. Est utilisé pour le référencement de l'image et l'accessibilité.",
     "field.blocks.gallery.caption.label" => "Légende",
-    "field.blocks.gallery.caption.help" => "S'affiche sous la photo",
+    "field.blocks.gallery.caption.help" => "S'affiche sous les photos",
 ];
